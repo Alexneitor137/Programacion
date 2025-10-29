6970efdc9ee30851b823413e1ce7a1444e49486d
Alexneitor137 <Alexcalderonsanchez@gmail.com>
2025-10-29 08:09:49 +0000

    Add files via upload

---
+++ Programacion/Interfaces graficas/4.Definimos accion.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def accion():
+    print("Has pulsado el boton, ahora tienes cancer FELICIDADES")
+
+ventana = tk.Tk()
+
+tk.Button(ventana,text="Pulsame si te atreves",command=accion).pack(padx=10,pady=10)
+
+ventana.mainloop() #Para que no se salga
