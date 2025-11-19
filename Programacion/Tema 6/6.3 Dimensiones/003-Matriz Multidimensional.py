9c8a45dfa56c601e911ef7687fb2e81ea86d74d1
Alexneitor137 <Alexcalderonsanchez@gmail.com>
2025-11-19 10:42:22 +0100

    Add files via upload

---
+++ Programacion/Tema 6/6.3 Dimensiones/003-Matriz Multidimensional.py
@@ -0,0 +1,16 @@
+agenda = []
+
+# Una lista dentro de otra
+agenda[0] = [
+    "Alejandro",
+    "Calderón Sánchez",
+    "alex@gmail.com",
+    "12345678"
+]
+
+agenda[1] = [
+    "Manuel",
+    "Serrano Lopez",
+    "manu@gmail.com",
+    "12345687"
+]
