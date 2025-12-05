1bf2cb7fdba1c4f8c26e118517d14fee79913cf1
Ajolote137 <Alexcalderonsanchez@gmail.com>
2025-12-05 09:24:40 +0100

    a

---
+++ Programacion/T.10/T.10.5/Bloques/cabecera.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <head>
+  </head>
+  <body>
+    <header>
+      <h1>Alejandro Calderón Sánchez</h1>
+      <h2>Página supercalifragilisticaespialidosa</h2>
+      <nav>
+        <a href="inicio.html">Inicio</a>
+        <a href="sobremi.html">Sobre mi</a>
+        <a href="contacto.html">Contacto</a>
+      </nav>
+    </header>
+    <main>
---
+++ Programacion/T.10/T.10.5/Bloques/pie.php
@@ -0,0 +1,5 @@
+<footer>
+      (c) 2025 Jose Vicente Carratala
+    </footer>
+  </body>
+</html>
