ccb9a49c5f664e7511d19fe881e7be0c5a4bb3fd
Ajolote137 <Alexcalderonsanchez@gmail.com>
2025-12-05 10:03:15 +0100

    Create 001-Panel de control.php

---
+++ Programacion/T.10/T.10.7 Bloques/001-Panel de control.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html>
+  <head>
+  </head>
+  <body>
+    <nav>
+    	<ul>
+      	<li>
+        	<a href="enlace.php">Enlace</a>
+        </li>
+        <li>
+        	<a href="enlace.php">Enlace</a>
+        </li>
+        <li>
+        	<a href="enlace.php">Enlace</a>
+        </li>
+        <li>
+        	<a href="enlace.php">Enlace</a>
+        </li>
+        <li>
+        	<a href="enlace.php">Enlace</a>
+        </li>
+        <li>
+        	<a href="enlace.php">Enlace</a>
+        </li>
+      </ul>
+    </nav>
+    <main>
+    	<table>
+      	<thead>
+        	<tr>
+          	<th>Nombre</th>
+            <th>Apellidos</th>
+            <th>Email</th>
+            <th>Direccion</th>
+          </tr>
+        </thead>
+        <tbody>
+        	<tr>
+          	<td>Nombre</td>
+            <td>Apellidos</td>
+            <td>Email</td>
+            <td>Direccion</td>
+          </tr>
+          <tr>
+          	<td>Nombre</td>
+            <td>Apellidos</td>
+            <td>Email</td>
+            <td>Direccion</td>
+          </tr>
+          <tr>
+          	<td>Nombre</td>
+            <td>Apellidos</td>
+            <td>Email</td>
+            <td>Direccion</td>
+          </tr>
+          <tr>
+          	<td>Nombre</td>
+            <td>Apellidos</td>
+            <td>Email</td>
+            <td>Direccion</td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  </body>
+</html>
