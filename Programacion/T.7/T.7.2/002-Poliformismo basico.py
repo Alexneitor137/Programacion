5b05d7feb3394bb39d65f532abcbc11f98a99708
Ajolote137 <Alexcalderonsanchez@gmail.com>
2025-12-05 09:12:52 +0100

    a

--- Programacion/T.7/T.7.2/002-Poliformismo basico.py
+++ Programacion/T.7/T.7.2/002-Poliformismo basico.py
@@ -8,10 +8,14 @@ class Persona():
 class Profesor(Persona):
   def __init__(self,nombre,apellidos):
   	super().__init__(nombre, apellidos)
+  def dameDatos(self):
+    return "Profesor: "+self.nombre+" "+self.apellidos
   
 class Alumno(Persona):
-  def __init__(self,nombre,apellidos,email,direccion):
+  def __init__(self,nombre,apellidos):
     super().__init__(nombre, apellidos)
+  def dameDatos(self):
+    return "Alumno: "+self.nombre+" "+self.apellidos
 
     
 alumno1 = Alumno("Alejandro","Calderon")
