1fb560b26cb2686d66ecaffbab8aaa68795388a0
Alexneitor137 <Alexcalderonsanchez@gmail.com>
2025-12-11 10:58:27 +0100

    s

--- Programacion/T.10/T.10.9/admin/escritorio.php
+++ Programacion/T.10/T.10.9/admin/escritorio.php
@@ -13,24 +13,7 @@
       <button>Autores</button>
     </nav>
     <main>
-    	<table>
-      	<thead>
-        	<tr>
-          	<th>Titulo</th><th>Texto</th><th>Fecha</th><th>Autor</th><th>Categoria</th>
-          </tr>
-        </thead>
-        <tbody>
-        	<?php
-          	for($i = 0;$i<20;$i++){
-            	echo '
-                <tr>
-                  <td>Titulo</td><td>Texto</td><td>Fecha</td><td>Autor</td><td>Categoria</td>
-                </tr>
-              	';
-            }
-          ?>
-        </tbody>
-      </table>
+    	<?php include "inc/read/leer.php"; ?>
     </main>
   </body>
 </html>
