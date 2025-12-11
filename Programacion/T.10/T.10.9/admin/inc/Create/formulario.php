a23cead22399b28b9d9bdeaa17b38cd2e90600b7
Alexneitor137 <Alexcalderonsanchez@gmail.com>
2025-12-11 12:14:10 +0100

    d

---
+++ Programacion/T.10/T.10.9/admin/inc/Create/formulario.php
@@ -0,0 +1,16 @@
+<form action="procesaformulario.php" method="POST">
+
+	<label for="titulo">Título de la nueva noticia</label>
+	<input type="text" name="titulo" id="titulo">
+  
+  <label for="contenido">Contenido de la nueva noticia</label>
+	<textarea id="contenido" name="contenido"></textarea>
+  
+  <label for="fecha_publicacion">Fecha de la nueva noticia</label>
+	<input type="text" name="fecha_publicacion" id="fecha_publicacion">
+  
+  <label for="autor_id">Autor de la nueva noticia</label>
+	<input type="text" name="autor_id" id="autor_id">
+  
+  <input type="submit">
+</form>
