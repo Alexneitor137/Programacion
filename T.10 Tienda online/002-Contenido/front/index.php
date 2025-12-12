8842c6a9c75b699ffe72a01017aea7b215a2e49c
Alexneitor137 <Alexcalderonsanchez@gmail.com>
2025-12-12 10:32:42 +0100

    Update index.php

--- T.10 Tienda online/002-Contenido/front/index.php
+++ T.10 Tienda online/002-Contenido/front/index.php
@@ -14,6 +14,8 @@
     color:white;
     justify-content:center;
     align-items:center;
+    border-radius:5px;
+    margin-bottom:20px;
   }
   #heroe a{
   	color:darkorchid;
@@ -22,6 +24,43 @@
     padding:10px;
     border-radius:5px;
   }
+</style>
+<section id="razones">
+	<article>
+  	<h4>Razon 1 por la cual debes comprar</h4>
+    <p>Descripción de esa razón</p>
+  </article>
+  <article>
+  	<h4>Razon 2 por la cual debes comprar</h4>
+    <p>Descripción de esa razón</p>
+  </article>
+  <article>
+  	<h4>Razon 3 por la cual debes comprar</h4>
+    <p>Descripción de esa razón</p>
+  </article>
+  <article>
+  	<h4>Razon 4 por la cual debes comprar</h4>
+    <p>Descripción de esa razón</p>
+  </article>
+</section>
+<style>
+	#razones{
+  	display:grid;
+    grid-template-columns:repeat(2,1fr);
+    gap:20px;
+  }
+  #razones article{
+  	text-align:center;
+    background:darkorchid;
+    padding:20px;
+    border-radius:5px;
+    display:flex;
+    flex-direction:column;
+    color:white;
+    justify-content:center;
+    align-items:center;
+  }
+  
 </style>
 
 <?php include "inc/piedepagina.php"; ?>
