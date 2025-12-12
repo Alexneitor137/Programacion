beecee9acc3dda64005e553c89471f02e44998cc
Alexneitor137 <Alexcalderonsanchez@gmail.com>
2025-12-12 10:19:57 +0100

    Update index.php

--- T.10 Tienda online/002-Contenido/front/index.php
+++ T.10 Tienda online/002-Contenido/front/index.php
@@ -1,3 +1,9 @@
 <?php include "inc/cabecera.php"; ?>
-Y aquí pongo "la crema"
+
+<section id="heroe">
+	<h3>Motivo por el cual debería comprar</h3>
+  <p>Frase sugerente al respecto</p>
+  <a href="catalogo.php">Vamos a ver esa maravilla de catálogo</a>
+</section>
+
 <?php include "inc/piedepagina.php"; ?>
