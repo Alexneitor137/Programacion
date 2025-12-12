d2f455ceab2aa9154a932e6117cb33d766762292
Alexneitor137 <Alexcalderonsanchez@gmail.com>
2025-12-12 09:25:08 +0100

    Update formularioactualizar.php

--- Programacion/T.10/T.10.9/admin/update/formularioactualizar.php
+++ Programacion/T.10/T.10.9/admin/update/formularioactualizar.php
@@ -24,17 +24,17 @@
 
       <div class="controlformulario">
         <label for="contenido">Contenido de la nueva noticia</label>
-        <textarea id="contenido" name="contenido"></textarea>
+        <textarea id="contenido" name="contenido"><?= $fila['contenido'] ?></textarea>
       </div>
 
       <div class="controlformulario">
         <label for="fecha_publicacion">Fecha de la nueva noticia</label>
-        <input type="text" name="fecha_publicacion" id="fecha_publicacion">
+        <input type="text" name="fecha_publicacion" id="fecha_publicacion" value="<?= $fila['fecha_publicacion'] ?>">
       </div>
 
       <div class="controlformulario">
         <label for="autor_id">Autor de la nueva noticia</label>
-        <input type="text" name="autor_id" id="autor_id">
+        <input type="text" name="autor_id" id="autor_id" value="<?= $fila['autor_id'] ?>">
       </div>
 
       <input type="submit">
