f83216f22d9d33dd943fd45c124068f7245ff4cd
Ajolote137 <Alexcalderonsanchez@gmail.com>
2026-01-27 11:05:03 +0100

    a

---
+++ Programacion/T.10/T.10.13-Idiomas en programacion/101-Ejercicios/003-Array interno.php
@@ -0,0 +1,31 @@
+<?php
+    $idioma['es']['inicio'] = "Inicio";
+    $idioma['es']['sobremi'] = "Sobre mi";
+    $idioma['es']['proyectos'] = "Proyectos";
+    $idioma['es']['contacto'] = "Contacto";
+  
+    $idioma['en']['inicio'] = "Home";
+    $idioma['en']['sobremi'] = "About me";
+    $idioma['en']['proyectos'] = "Projects";
+    $idioma['en']['contacto'] = "Contact";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Multi idioma</title>
+</head>
+<body>
+    <select>
+        <option value="es">🇪🇸</option>
+        <option value="en">🇬🇧</option>
+    </select>
+    <h1>Alejandro Calderón Sánchez</h1>
+    <nav>
+        <a href="">Inicio</a>
+        <a href="">Sobre mi</a>
+        <a href="">Proyectos</a>
+        <a href="">Contacto</a>
+    </nav>
+</body>
+</html>
